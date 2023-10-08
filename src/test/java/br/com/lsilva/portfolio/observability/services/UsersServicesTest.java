4cffddd48267c1b80d2598f7ff177ac98a939cd0
LuizPauloS <luiz.paulo.09@hotmail.com>
2023-10-08 14:20:30 -0400

    adicionado step de execucao de teste unitario

---
+++ src/test/java/br/com/lsilva/portfolio/observability/services/UsersServicesTest.java
@@ -0,0 +1,16 @@
+package br.com.lsilva.portfolio.observability.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class UsersServicesTest {
+    
+
+    @Test
+    public void teste() throws Exception{
+        throw new Exception("ERRO");
+    }
+
+}
